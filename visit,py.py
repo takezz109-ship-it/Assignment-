2165fbd561b2521e7173346bfafd80cb41569f80
takezz109-ship-it <takezz109@gmail.com>
2025-11-20 22:23:59 +0100

    Add files via upload

---
+++ visit,py.py
@@ -0,0 +1,25 @@
+#student name: Mbedika Malvin Musi
+#department: software engineering
+
+places=["london, egypt, tanziana, america"]
+print("london, egypt, tanziana, americsa")
+print(places) 
+
+result = sorted(places)
+print(result)
+print(places) 
+places=["london, egypt, tanziana, america"]
+places.reverse()
+places=["london, egypt, tanziana. america"]
+rev = places[::-1]
+original = rev[::-1]
+print(places)
+
+places=["london, egypt, tanziana, america"]
+places.sort()
+print(places) 
+
+places=["london; egypt, tanziana, america "]
+places.sort(reverse ="london" )
+print(places)
+
